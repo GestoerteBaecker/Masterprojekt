32926c2b722bcc1aa9eabc9fd8e7d823ae1e4b6d
GestoerteBaecker <8denimdmein8>
2020-10-27 17:11:43 +0100

    Signal bei veraenderter Variabel

--- Sensoren/variabel_change_event.py
+++ Sensoren/variabel_change_event.py
@@ -1,30 +1,23 @@
-
 import time
 
-class ObjectHoldingTheValue:
+
+class ObjectHoldingTheValue(object):
     def __init__(self, initial_value=0):
         self._value = initial_value
-        self._callbacks = []
 
-    @property
-    def value(self):
-        return self._value
 
-    @value.setter
-    def value(self, new_value):
-        old_value = self._value
-        self._value = new_value
-        self._notify_observers(old_value, new_value)
-
-    def _notify_observers(self, old_value, new_value):
-        for callback in self._callbacks:
-            callback(old_value, new_value)
-
-    def register_callback(self, callback):
-        self._callbacks.append(callback)
+    def __setattr__(self, key, value):
+        try:
+            old_var = self._value
+        except:
+            pass
+        super().__setattr__(key, value)
+        if key == "_value":
+            print(old_var, value)
 
 
 a = ObjectHoldingTheValue(10)
-print(a.x)
-time.sleep(10)
-a.x = 50
+time.sleep(2)
+a._value = 50
+
+# https://stackoverflow.com/questions/12998926/clean-way-to-disable-setattr-until-after-initialization

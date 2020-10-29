f915e6344333b4d5ac3eb471834bc4e3a1b88914
GestoerteBaecker <8denimdmein8>
2020-10-29 16:52:36 +0100

    Messgebiet ergaenzt

--- Sensoren/Messgebiet.py
+++ Sensoren/Messgebiet.py
@@ -2,7 +2,12 @@ import Sensoren
 
 class Messprogramm:
 
-    def __init__(self):
+    def __init__(self, initale_position, initiale_ausdehnung, auflösung):
+        """
+        :param initale_position: Mittige Position des zu vermessenden Gebiets (in utm), um das sich der Quadtree legen soll
+        :param initiale_ausdehnung: grobe Ausdehnung in Meter
+        :param auflösung:
+        """
         self.quadtree = None
 
     def daten_einspeisen(self, punkt, datenpaket):

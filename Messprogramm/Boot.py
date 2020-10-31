cf7041d3c4fd5930a59681c7667a7834de557eee
GestoerteBaecker <gestoerte_baecker@hotmail.com>
2020-10-31 16:22:32 +0100

    Struktur abgeaendert

--- Messprogramm/Boot.py
+++ Messprogramm/Boot.py
@@ -5,17 +5,19 @@ import threading
 import time
 
 # Klasse, die alle Funktionalitäten des Bootes umfasst
+# self.auslesen > self.fortlaufende_aktualisierung > self.datenbankbeschreiben
+# -> d.h. damit zB self. datenbankbeschreiben True ist müssen mind. die anderen beiden auch True sein
 class Boot:
 
     def __init__(self,GNSS1_COM="COM0", GNSS1_baud=0, GNSS1_timeout=0, GNSS1_takt=0.2, GNSS2_COM="COM0", GNSS2_baud=0, GNSS2_timeout=0, GNSS2_takt=0.2, ECHO_COM="COM0", ECHO_baud=0, ECHO_timeout=0, ECHO_takt=0.2, DIST_COM="COM0", DIST_baud=0, DIST_timeout=0, DIST_takt=1):
 
-        self.fortlaufende_aktualisierung = False        # Schlater, ob das Dict mit den aktuellen Sensordaten permanent aktualisiert wird
         self.auslesen = False                           # Schalter, ob die Sensoren dauerhaft ausgelesen werden
-        self.datenbankbeschreiben = False               # Schlater, ob die Datenbank mit Sensordaten beschrieben wird
-        self.AktuelleSensordaten = {}
-        self.Sensorliste = []
-        self.AktuelleSensordaten = {} # hier stehen die Daten-Objekte mit den jeweiligen Sensorennamen (wie GNSS1) als Schlüssekwörter drin
-        self.Sensornamen = []
+        self.fortlaufende_aktualisierung = False        # Schalter, ob das Dict mit den aktuellen Sensordaten permanent aktualisiert wird
+        self.datenbankbeschreiben = False               # Schalter, ob die Datenbank mit Sensordaten beschrieben wird
+        self.Sensorliste = [] # hier sind die Sensor-Objekte drin
+        self.AktuelleSensordaten = [] # hier stehen die Daten-Objekte drin
+        self.Sensornamen = [] # hier sind die Namen der Sensoren in der Reihenfolge wie in self.Sensorliste drin
+        self.aktualisierungsprozess = None # Thread mit Funktion, die die Sensordaten innerhalb dieser Klasse speichert
 
         if GNSS1_COM != "COM0":
             self.GNSS1 = Sensoren.GNSS(GNSS1_COM, GNSS1_baud, GNSS1_timeout, GNSS1_takt)
@@ -37,88 +39,79 @@ class Boot:
             self.Sensorliste.append(self.DIST)
             self.Sensornamen.append("Distanz")
 
+        #TODO: Verbindung mit Pixhawk
 
+        self.AktuelleSensordaten = len(self.Sensorliste) * [None]
+
+
+    # muss einmalig angestoßen werden und verbleibt im Messzustand, bis self.auslesen auf False gesetzt wird
     def Sensorwerte_auslesen(self):
 
         if not self.auslesen:
             self.auslesen = True
-            for Sensor in self.Sensorliste:
-                Sensor.read_datastream()
+            for sensor in self.Sensorliste:
+                sensor.read_datastream()
 
-            self.Datenaktualisierung()  # Funktion zum dauerhaften Überschreiben des aktuellen Zustands (neuer Thread wir aufgemacht)
 
-    def Punkt_anfahren(self, e, n):
-        pass
-
-    def Datenbank_beschreiben(self):
-
-        self.Verbinden_mit_DB()
-
-        if not self.auslesen:
-            self.Sensorwerte_auslesen()
-            self.auslesen = True
-
-        if not self.datenbankbeschreiben:
-            for Sensor in self.Sensorliste:
-                Sensor.start_pushing_db()       # Daten permanent in Datenbank ablegen
-            self.datenbankbeschreiben = True
+    # muss einmalig angestoßen werden
+    def Datenbank_beschreiben(self, mode=0):
+        """
+        :param mode: 0 für eine DB-Tabelle, in der alle Daten als ein einziger Eintrag eingeführt werden
+            1 für separate DB-Tabellen je Sensor (ursprüngliches Vorhaben)
+        """
+        self.Verbinden_mit_DB(mode)
 
         if not self.fortlaufende_aktualisierung:
             self.Datenaktualisierung()  # Funktion zum dauerhaften Überschreiben des aktuellen Zustands (neuer Thread wir aufgemacht)
 
-    def Verbinden_mit_DB(self):
+        if mode == 0:
+            pass
 
-        for i in range(0,len(self.Sensorliste)):
-            try:
-                self.Sensorliste[i].connect_to_db(self.Sensornamen[i])
-            except:
-                print("Für" + self.Sensornamen[i] + "konnte keine Datenbanktabelle angelegt werden")
+        elif mode == 1:
+            if not self.datenbankbeschreiben:
+                for Sensor in self.Sensorliste:
+                    Sensor.start_pushing_db()       # Daten permanent in Datenbank ablegen
+                self.datenbankbeschreiben = True
+
+    def Verbinden_mit_DB(self, mode=0):
+        """
+        :param mode: 0 für eine DB-Tabelle, in der alle Daten als ein einziger Eintrag eingeführt werden
+            1 für separate DB-Tabellen je Sensor (ursprüngliches Vorhaben)
+        """
+        if mode == 0:
+            pass
+
+        elif mode == 1:
+            for i, sensor in enumerate(self.Sensorliste):
+                try:
+                    sensor.connect_to_db(self.Sensornamen[i])
+                except:
+                    print("Für " + self.Sensornamen[i] + " konnte keine Datenbanktabelle angelegt werden")
 
     def Datenaktualisierung(self):
 
+        if not self.auslesen:
+            self.Sensorwerte_auslesen()
+
         self.fortlaufende_aktualisierung = True
 
-        def Uebderscheibungsfunktion(self):
-
+        def Ueberscheibungsfunktion(self):
             while self.fortlaufende_aktualisierung:
                 for i in range(0, len(self.Sensorliste)):
-                    Sensor = self.Sensorliste[i]
-                    Sensorname = self.Sensornamen[i]
+                    sensor = self.Sensorliste[i]
+                    self.AktuelleSensordaten[i] = sensor.aktdaten
+                time.sleep(0.1)
 
-                    typ = type(Sensor).__name__
-
-                    if typ == "GNSS":
-
-                        x = Sensor.aktdaten.daten[0]
-                        y = Sensor.aktdaten.daten[1]
-                        hdop = Sensor.aktdaten.daten[2]
-                        h = Sensor.aktdaten.daten[4]
-                        gps_status = Sensor.aktdaten.daten[5]
-
-                        self.AktuelleSensordaten = {Sensorname+"x": x, Sensorname+"y": y, Sensorname+"hdop": hdop, Sensorname+"h": h, Sensorname+"gps_status": gps_status}
-
-                    if typ == "Distanzmesser":
-
-                        dist = Sensor.aktdaten.deten[0]
-
-                        self.AktuelleSensordaten = {Sensorname+"dist": dist}
-
-                    if typ == "Echolot":
-
-                        tiefe1 = Sensor.aktdaten.daten[0]
-                        tiefe2 = Sensor.aktdaten.daten[1]
-
-                        self.AktuelleSensordaten = {Sensorname+"tiefe1": tiefe1, Sensorname+"tiefe2": tiefe2}
-
-                time.sleep(1)
-
-        self.writing_process = threading.Thread(target=Uebderscheibungsfunktion, args=(self, ), daemon=True)
-        self.writing_process.start()
+        self.aktualisierungsprozess = threading.Thread(target=Ueberscheibungsfunktion, args=(self, ), daemon=True)
+        self.aktualisierungsprozess.start()
 
     def Hinderniserkennung(self):
         pass
 
     def Erkunden(self, Art_d_Gewaessers): # Art des Gewässers optional
+        pass
+
+    def Punkt_anfahren(self, e, n):
         pass
 
     def Wegberechnung(self):

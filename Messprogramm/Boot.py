8b5b49995f7305cb37ca45f8fcf4b6835861c554
Eilers <ma5534@hs-woe.de>
2020-11-02 18:07:26 +0100

    Boot und PixHawk

--- Messprogramm/Boot.py
+++ Messprogramm/Boot.py
@@ -12,15 +12,15 @@ import time
 # -> d.h. damit zB self. datenbankbeschreiben True ist müssen mind. die anderen beiden auch True sein
 class Boot:
 
-    def __init__(self,GNSS1_COM="COM0", GNSS1_baud=0, GNSS1_timeout=0, GNSS1_takt=0.2, GNSS2_COM="COM0", GNSS2_baud=0, GNSS2_timeout=0, GNSS2_takt=0.2, ECHO_COM="COM0", ECHO_baud=0, ECHO_timeout=0, ECHO_takt=0.2, DIST_COM="COM0", DIST_baud=0, DIST_timeout=0, DIST_takt=1):
+    def __init__(self,Pix_COM="com0", GNSS1_COM="COM0", GNSS1_baud=0, GNSS1_timeout=0, GNSS1_takt=0.2, GNSS2_COM="COM0", GNSS2_baud=0, GNSS2_timeout=0, GNSS2_takt=0.2, ECHO_COM="COM0", ECHO_baud=0, ECHO_timeout=0, ECHO_takt=0.2, DIST_COM="COM0", DIST_baud=0, DIST_timeout=0, DIST_takt=1):
 
         self.auslesen = False                           # Schalter, ob die Sensoren dauerhaft ausgelesen werden
         self.fortlaufende_aktualisierung = False        # Schalter, ob das Dict mit den aktuellen Sensordaten permanent aktualisiert wird
         self.datenbankbeschreiben = False               # Schalter, ob die Datenbank mit Sensordaten beschrieben wird
-        self.Sensorliste = [] # hier sind die Sensor-Objekte drin
-        self.AktuelleSensordaten = [] # hier stehen die Daten-Objekte drin
-        self.Sensornamen = [] # hier sind die Namen der Sensoren in der Reihenfolge wie in self.Sensorliste drin
-        self.aktualisierungsprozess = None # Thread mit Funktion, die die Sensordaten innerhalb dieser Klasse speichert
+        self.Sensorliste = []                           # hier sind die Sensor-Objekte drin
+        self.AktuelleSensordaten = []                   # hier stehen die Daten-Objekte drin
+        self.Sensornamen = []                           # hier sind die Namen der Sensoren in der Reihenfolge wie in self.Sensorliste drin
+        self.aktualisierungsprozess = None              # Thread mit Funktion, die die Sensordaten innerhalb dieser Klasse speichert
         self.datenbankbeschreiben_thread = None
         self.db_verbindung = None
         self.db_zeiger = None
@@ -29,6 +29,9 @@ class Boot:
         self.db_id = 0
         takt = [GNSS1_takt, GNSS2_takt, ECHO_takt, DIST_takt]
         self.db_takt = min(*takt)
+
+        if Pix_COM != "com0":
+            self.PixHawk = Pixhawk.Pixhawk(Pix_COM)
 
         if GNSS1_COM != "COM0":
             self.GNSS1 = Sensoren.GNSS(GNSS1_COM, GNSS1_baud, GNSS1_timeout, GNSS1_takt)
@@ -49,8 +52,6 @@ class Boot:
             self.DIST = Sensoren.Distanzmesser(DIST_COM, DIST_baud, DIST_timeout, DIST_takt)
             self.Sensorliste.append(self.DIST)
             self.Sensornamen.append("Distanz")
-
-        #TODO: Verbindung mit Pixhawk
 
         self.AktuelleSensordaten = len(self.Sensorliste) * [None]
 
@@ -146,24 +147,27 @@ class Boot:
 
         self.fortlaufende_aktualisierung = True
 
-        def Ueberscheibungsfunktion(self):
+        def Ueberschreibungsfunktion(self):
             while self.fortlaufende_aktualisierung:
                 for i in range(0, len(self.Sensorliste)):
                     sensor = self.Sensorliste[i]
                     self.AktuelleSensordaten[i] = sensor.aktdaten
                 time.sleep(self.takt)
 
-        self.aktualisierungsprozess = threading.Thread(target=Ueberscheibungsfunktion, args=(self, ), daemon=True)
+        self.aktualisierungsprozess = threading.Thread(target=Ueberschreibungsfunktion, args=(self, ), daemon=True)
         self.aktualisierungsprozess.start()
 
     def Hinderniserkennung(self):
         pass
 
-    def Erkunden(self, Art_d_Gewaessers): # Art des Gewässers optional
+    def Erkunden(self, Art_d_Gewaessers):   # Art des Gewässers (optional)
         pass
 
-    def Punkt_anfahren(self, e, n):
-        pass
+    def Punkt_anfahren(self, e, n, geschw = 2):  # Utm-Koordinaten und Gechwindigkeit setzen
+
+        self.PixHawk.Geschwindigkeit_setzen(geschw)
+        self.PixHawk.Wegpunkt_anfahren(e, n)
+        print("Fahre Punkt mit Koordinate E:", e, "N:", n, "an")
 
     def Wegberechnung(self):
         pass
@@ -176,8 +180,11 @@ class Boot:
         for Sensor in self.Sensorliste:
             Sensor.kill()
 
+        self.PixHawk.Trennen()
+
     def RTL(self):
-        pass
+
+        self.PixHawk.Return_to_launch()
 
     def Kalibrierung(self):
         pass
@@ -244,7 +251,7 @@ def Flächenberechnung(x, y):
 # Zum Testen
 if __name__=="__main__":
 
-    Boot = Boot(GNSS1_COM="COM10", GNSS1_baud=115200, GNSS1_timeout=0, GNSS1_takt=0.2, GNSS2_COM="COM11", GNSS2_baud=115200, GNSS2_timeout=0, GNSS2_takt=0.2, ECHO_COM="COM1", ECHO_baud=19200, ECHO_timeout=0, ECHO_takt=0.2, DIST_COM="COM12", DIST_baud=19200, DIST_timeout=0, DIST_takt=1)
+    Boot = Boot(Pix_COM="com0", GNSS1_COM="COM10", GNSS1_baud=115200, GNSS1_timeout=0, GNSS1_takt=0.2, GNSS2_COM="COM11", GNSS2_baud=115200, GNSS2_timeout=0, GNSS2_takt=0.2, ECHO_COM="COM1", ECHO_baud=19200, ECHO_timeout=0, ECHO_takt=0.2, DIST_COM="COM12", DIST_baud=19200, DIST_timeout=0, DIST_takt=1)
 
     Boot.Sensorwerte_auslesen()
     time.sleep(5)
--- Messprogramm/Pixhawk.py
+++ Messprogramm/Pixhawk.py
@@ -1,30 +1,66 @@
-from dronekit import connect, VehicleMode, LocationGlobal
+import dronekit
 import utm
+import time
 
 # Klasse zum Ansteuern der Motoren
 class Pixhawk:
 
-    def __init__(self,COM="COM0"):
+    def __init__(self, COM):
 
         self.connection_string = COM
         self.vehicle = ""
+        self.verbindung_hergestellt = False
+        self.initialisierung = False
+
+        try:
+            self.Verbinden()
+
+        except:
+            print("Es konnte keine Verbindung mit dem PixHawk hergestellt werden")
+
+        while not self.vehicle.is_armable:              # Warten, bis der PixHawk bereit zum initialisieren ist
+            time.sleep(1)                               # Homeposition wird automatisch bei einer FIX-GNSS-Lösung erzeugt
+
+        try:
+            self.Initialisieren()
+            print("PixHawk verbunden und initialisiert")
+
+        except:
+            print("PixHawk konnte nicht initialisiert werden")
+
 
     def Verbinden(self):
 
-        self.vehicle = connect(self.connection_string, wait_ready=True)
+        self.vehicle = dronekit.connect(self.connection_string, wait_ready=True)
+        self.verbindung_hergestellt = True
+
+    def Initialisieren(self):
+
         self.vehicle.initialize()
+        self.vehicle.armed = True
+        self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+        self.initialisierung = True
 
     def Geschwindigkeit_setzen(self, v):  # v = Geschwindigkeit im m/s
 
-        self.vehicle.groundspeed(v)
-
+        self.vehicle.groundspeed = v
 
     def Wegpunkt_anfahren(self, utm_x, utm_y):
 
-        wp = LocationGlobal(utm.to_latlon(utm_x, utm_y, 32, 'U'))
-        self.vehicle.simple_goto(wp)
+        if self.initialisierung:
+            wp = dronekit.LocationGlobal(utm.to_latlon(utm_x, utm_y, 32, 'U'), 0)
+            self.vehicle.simple_goto(wp)
+        else:
+            try:    self.Initialisieren()
+            except: print("PixHawk konnte nicht initialisiert werden")
 
-    def Retrun_to_launche:
-        pass
+            if self.initialisierung: self.Wegpunkt_anfahren()
 
+    def Return_to_launch(self):
+
+        self.vehicle.mode = dronekit.VehicleMode("RTL")
+
+    def Trennen(self):
+
+        self.vehicle.close()
 
